fc8822b96de9be960f260c7e3f59ec9941b7ebbf
gefeeHome <megamisha41521@gmail.com>
2025-12-01 16:26:46 +0300

    del helper, change /register endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,51 +5,20 @@ from sqlmodel import Session, select
 from datetime import datetime
 
 from app.database import get_session
-from app.models import User, UserCreate, UserRead
+from app.models import User, UserCreate, UserRead, Statistics
 from app.utils import (
     hash_password, verify_password, create_access_token,
     decode_token, create_verification_token, send_verification_email
 )
+from app.routers.helper import get_current_user
+
 from app.logger import logger
 
 router = APIRouter()
 security = HTTPBearer()
 
 
-# ---------- Helpers ----------
-async def get_current_user(
-    credentials: HTTPAuthorizationCredentials = Depends(security),
-    session: Session = Depends(get_session)
-) -> User:
-    token = credentials.credentials
-    logger.info(f"Получен токен для проверки: {token}")
 
-    data = decode_token(token)
-    if not data:
-        logger.warning("Неверный или просроченный токен")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-        )
-
-    user_id = data.get("user_id")
-    if not user_id:
-        logger.warning("Некорректный payload токена")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token payload",
-        )
-
-    user = session.get(User, user_id)
-    if not user:
-        logger.warning(f"Пользователь с id {user_id} не найден")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User not found",
-        )
-
-    logger.info(f"Авторизован пользователь: {user.email}")
-    return user
 
 
 # ---------- Endpoints ----------
@@ -79,6 +48,9 @@ def register(user_data: UserCreate, session: Session = Depends(get_session)):
         session.add(new_user)
         session.commit()
         session.refresh(new_user)
+        stats = Statistics(user_id=new_user.id)
+        session.add(stats)
+        session.commit()
 
         logger.info(f"Пользователь создан: {new_user.email}, отправка письма подтверждения")
 
